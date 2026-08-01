baa4002b9d540705cd0ae62b2ae9c3d89fac3cfa
Jonaphan-Mareno <jonamareno111@gmail.com>
2026-08-01 17:41:29 +0200

    Implement a add task feature

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
-const app = express();
+const app = express();app.use(express.json());
 const port = 3000;
 const db = new sqlite3.Database('./my-database.db');
 db.serialize(() => {
   // Create a table for users
   db.run("CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, title TEXT, Description TEXT,Due_date TEXT,Topic TEXT,status TEXT)");
-  db.run("ALTER TABLE tasks ADD status TEXT")
   // Clear old data and insert a couple of names for testing
   db.run("DELETE FROM tasks");
 db.run("INSERT INTO tasks (title, Description, Due_date, Topic,status) VALUES ('Buy groceries', 'Get milk and eggs', 'Today', 'Errands','Todo')");
@@ -22,10 +21,23 @@ app.get('/api/tasks', (req, res) => {
       res.status(500).json({ error: err.message });
       return;
     }
+
     // Send the database rows back to the frontend
     res.json(rows);
   });
 });
+app.post('/api/addtasks',(req, res)=>{
+    const { title, Description, Due_date, Topic, status } = req.body;
+ const query = "INSERT INTO tasks (title, Description, Due_date, Topic, status) VALUES (?, ?, ?, ?, ?)";
+       db.run(query, [title, Description, Due_date, Topic, status], function(err) {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        // 3. Send a success message back so the browser knows it finished
+        res.json({ message: "Task added successfully!", id: this.lastID });
+    }); 
+    });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port} in your browser lil bro.`);
